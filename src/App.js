6f34864cfbc6c7427aa821ee739a9bd1d3060ad3
Codebender <developingcoder@gmail.com>
2018-10-06 17:52:34 +0300

    create site blueprint according to FCC guidelines

--- src/App.js
+++ src/App.js
@@ -5,9 +5,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <NavBar />
+        <Introduction />
+        <Projects />
       </div>
     );
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav id="navbar">
+        <a href="github.com" id="profile-link">GitHub</a>
+        <a href="#welcome-section">Introduction</a>
+        <a href="#projects">Projects</a>
+      </nav>
+    )
+  }
+}
+
+class Introduction extends Component {
+  render() {
+    return (
+      <section id="welcome-section">
+        <h1>Welcome</h1>
+      </section>
+    )
+  }
+}
+
+class Projects extends Component {
+  render() {
+    return (
+      <section id="projects">
+        <div className="project-tile">
+          <a href="google.com">Example project</a>
+        </div>
+      </section>
+    )
   }
 }
 
