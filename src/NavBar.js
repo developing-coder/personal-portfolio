ac20d6018f64635b0c23bdce1cb07fcdf487f3d2
Codebender <developingcoder@gmail.com>
2018-10-13 11:53:45 +0300

    Arrange NavBar into a horizontal layout

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
@@ -7,21 +10,39 @@ import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 library.add(faGithub, faTwitter);
 
 
-export default class NavBar extends Component {
+const styles = {
+    root: {
+      flexGrow: 1
+    }
+  };
+
+
+class NavBar extends Component {
+
     render() {
+        const { classes } = this.props
+
         return (
-            <div>
-            <AppBar position="static" id="navbar">
-                <a href="https://github.com/developing-coder/" id="profile-link">
-                <FontAwesomeIcon icon={["fab", "github"]}/>
-                </a>
-                <a href="https://twitter.com/developingcoder" id="twitter-profile-link">
-                <FontAwesomeIcon icon={["fab", "twitter"]}/>
-                </a>
-                <a href="#welcome-section">Introduction</a>
-                <a href="#projects">Projects</a>
-            </AppBar>
+            <div className={classes.root}>
+                <AppBar position="static" id="navbar">
+                    <Toolbar>
+                        <a href="https://github.com/developing-coder/" id="profile-link">
+                            <FontAwesomeIcon icon={["fab", "github"]} />
+                        </a>
+                        <a href="https://twitter.com/developingcoder" id="twitter-profile-link">
+                            <FontAwesomeIcon icon={["fab", "twitter"]} />
+                        </a>
+                        <a href="#welcome-section">Introduction</a>
+                        <a href="#projects">Projects</a>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
 }
+
+NavBar.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(NavBar);
