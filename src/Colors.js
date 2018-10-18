5d16774a97c02aee11f2dd05ead5ef80325d68db
Codebender <developingcoder@gmail.com>
2018-10-18 12:15:08 +0300

    Add a separate file for storing color codes.

---
+++ src/Colors.js
@@ -0,0 +1,10 @@
+export const themeColors = {
+    primary: {
+        light: 'white',
+        main: '#212121',
+        dark: '#212121',
+        contrastText: '#8BC34A',
+      },
+      secondary: 'purple',
+      error: 'red'
+}
